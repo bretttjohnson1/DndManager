ad5f38fe9f3af3ed9a218a2ae74b3d5a4d524f96
btj12 <swimbrettjohnson@gmail.com>
2017-12-01 23:50:05 -0500

    Fixes tables

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(User)
+admin.site.register(Static_Race)
+admin.site.register(Game)
+admin.site.register(Character)
+admin.site.register(Inventory)
+admin.site.register(Weapon)
+admin.site.register(Armor)
+admin.site.register(Base_Stats)
+admin.site.register(Feats)
+admin.site.register(Skills)
+
 
 # Register your models here.
--- website/models.py
+++ website/models.py
@@ -4,25 +4,35 @@ from django.db import models
 
 class Static_Race(models.Model):
     race_name = models.CharField(max_length=40, primary_key=True)
-    strmod = models.IntegerField
-    dexmod = models.IntegerField
-    conmod = models.IntegerField
-    intmod = models.IntegerField
-    wismod = models.IntegerField
-    chamod = models.IntegerField
+    strmod = models.IntegerField(default=0)
+    dexmod = models.IntegerField(default=0)
+    conmod = models.IntegerField(default=0)
+    intmod = models.IntegerField(default=0)
+    wismod = models.IntegerField(default=0)
+    chamod = models.IntegerField(default=0)
+    class Meta:
+        db_table = "static_race"
+    def __str__(self):
+        return self.race_name
 
 
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
     account_name = models.CharField(max_length=40)
     password_hash = models.CharField(max_length=40)
+    class Meta:
+        db_table = "users"
+    def __str__(self):
+        return self.account_name
 
 class Game(models.Model):
     game_id = models.AutoField(primary_key=True)
     game_name = models.CharField(max_length=40)
     ran_by = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
+    class Meta:
+        db_table = "games"
+    def __str__(self):
+        return self.game_name
 
 class Character(models.Model):
     char_id = models.AutoField(primary_key=True)
@@ -31,53 +41,70 @@ class Character(models.Model):
     game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
     race = models.ForeignKey(Static_Race)
     classname = models.CharField(max_length=40)
-    level = models.IntegerField
+    level = models.IntegerField(default=0)
+    class Meta:
+        db_table = "characters"
+    def __str__(self):
+        return self.name
 
 
 class Inventory(models.Model):
     char_id = models.ForeignKey(Character, on_delete=models.CASCADE)
     item_name = models.CharField(max_length=40)
     item_desc = models.CharField(max_length=40)
-    quantity = models.IntegerField
+    quantity = models.IntegerField(default=0)
 
     class Meta:
         unique_together = (("char_id", "item_name"))
+        db_table = "inventories"
+    def __str__(self):
+        return self.item_name
 
 
 class Weapon(models.Model):
     char_id = models.ForeignKey(Character, on_delete=models.CASCADE)
     name = models.CharField(max_length=40)
-    number_damage_dice = models.IntegerField
-    type_damage_dice = models.IntegerField
-    damage_bonus = models.IntegerField
+    number_damage_dice = models.IntegerField(default=0)
+    type_damage_dice = models.IntegerField(default=0)
+    damage_bonus = models.IntegerField(default=0)
     critical = models.CharField(max_length=40)
     type = models.CharField(max_length=40)
     range = models.CharField(max_length=40)
-    quantity = models.IntegerField
+    quantity = models.IntegerField(default=0)
     desc = models.CharField(max_length=4000)
     class Meta:
         unique_together = (("char_id", "name"))
+        db_table = "weapons"
+    def __str__(self):
+        return self.name
 
 
 class Armor(models.Model):
     char_id = models.ForeignKey(Character, on_delete=models.CASCADE)
     name = models.CharField(max_length=40)
-    armor_bonus = models.IntegerField
-    armor_check_penalty = models.IntegerField
+    armor_bonus = models.IntegerField(default=0)
+    armor_check_penalty = models.IntegerField(default=0)
     type = models.CharField(max_length=40)
     desc = models.CharField(max_length=4000)
     class Meta:
         unique_together = (("char_id", "name"))
+        db_table = "armors"
+    def __str__(self):
+        return self.name
 
 
 class Base_Stats(models.Model):
     char_id = models.ForeignKey(Character, primary_key=True, on_delete=models.CASCADE)
-    str = models.IntegerField
-    dex = models.IntegerField
-    con = models.IntegerField
-    int = models.IntegerField
-    wis = models.IntegerField
-    cha = models.IntegerField
+    str = models.IntegerField(default=0)
+    dex = models.IntegerField(default=0)
+    con = models.IntegerField(default=0)
+    int = models.IntegerField(default=0)
+    wis = models.IntegerField(default=0)
+    cha = models.IntegerField(default=0)
+    class Meta:
+        db_table = "base_stats"
+    def __str__(self):
+        return str(self.char_id)
 
 
 class Feats(models.Model):
@@ -86,6 +113,9 @@ class Feats(models.Model):
     desc = models.CharField(max_length=400)
     class Meta:
         unique_together = (("char_id", "name"))
+        db_table = "feats"
+    def __str__(self):
+        return self.name
 
 
 class Skills(models.Model):
@@ -101,9 +131,12 @@ class Skills(models.Model):
 
     ]
     relevant_ability = models.CharField(max_length=3, choices=abilit_choices)
-    ranks = models.IntegerField
-    class_mod = models.IntegerField
-    race_mod = models.IntegerField
+    ranks = models.IntegerField(default=0)
+    class_mod = models.IntegerField(default=0)
+    race_mod = models.IntegerField(default=0)
     class Meta:
         unique_together = (("char_id", "skill_name"))
+        db_table = "skills"
+    def __str__(self):
+        return self.name
 
